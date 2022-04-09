1d580f4bae3bbab762f4278ea701438b728de013
LittleToes <kristina.r.brennan@gmail.com>
2022-04-09 16:45:23 -0500

    displays searched cities including newest search

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-console.log("I'm in");
 var apiKey = "2fba4a1671bedcc3d7eb87766d0af9e8";
-
+var storedCities = localStorage.getItem("cities");
+var searchedCities = document.getElementById("cityResults");
 
 document.getElementById("searchForm").addEventListener("submit", function(event) {
     event.preventDefault();
     var city = document.getElementById("cityInput").value;
+    var storedCities = localStorage.getItem("cities");
+    var li = document.createElement('li');
+    // clear out input field
     document.getElementById("cityInput").value = '';
 
-    var storedCities = localStorage.getItem("cities");
+    //create a li item displaying the most recent search
+    li.innerText = city;
+    searchedCities.appendChild(li);
+
+    //save items to localstorage
     if (!storedCities) {
-        console.log("yeah");
         localStorage.setItem("cities", JSON.stringify([{city: city}]));
         return;
     }
     storedCities = JSON.parse(storedCities);
-    console.log (storedCities);
     storedCities.push({city: city});
-    console.log(storedCities);
     localStorage.setItem("cities", JSON.stringify(storedCities));
+});
 
-})
+// display previous searches on page load
+function displaySearches() {
+    storedCities = JSON.parse(storedCities);
+    if (storedCities) {
+        for (var i=0; i<storedCities.length; i++) {
+            var li = document.createElement('li');
+            li.innerText = storedCities[i].city;
+            searchedCities.appendChild(li);
+        }
+    }
+};
+
+displaySearches();
+
+//get coordinates of searched city
+// function getLocation(city)
