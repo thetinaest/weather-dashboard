bb68a832fcbc95050ec6522866ada7808337d82a
LittleToes <kristina.r.brennan@gmail.com>
2022-04-10 11:16:34 -0500

    display current day's weather for searched city

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # weather-dashboard
-GIVEN a weather dashboard with form inputs
+<!-- GIVEN a weather dashboard with form inputs
 WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
+THEN I am presented with current and future conditions for that city and that city is added to the search history -->
 WHEN I view current weather conditions for that city
 THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 WHEN I view the UV index
--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
     li.innerText = city;
     searchedCities.appendChild(li);
 
+    getLocation(city);
+
     //save items to localstorage
     if (!storedCities) {
         localStorage.setItem("cities", JSON.stringify([{city: city}]));
@@ -22,6 +24,7 @@ document.getElementById("searchForm").addEventListener("submit", function(event)
     storedCities = JSON.parse(storedCities);
     storedCities.push({city: city});
     localStorage.setItem("cities", JSON.stringify(storedCities));
+
 });
 
 // display previous searches on page load
@@ -36,7 +39,60 @@ function displaySearches() {
     }
 };
 
-displaySearches();
-
 //get coordinates of searched city
-// function getLocation(city)
+function getLocation(city) {
+    var locationUrl = "https://api.openweathermap.org/geo/1.0/direct?q="+ city + "&limit=1&appid=" + apiKey;
+    fetch(locationUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        if (data.length !== 0) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&exclude=minutely,hourly,alerts&appid=" + apiKey + "&units=imperial";
+            getWeather(weatherUrl, city);
+            console.log(weatherUrl);
+        } else {
+            alert("City not found. Please try another search");
+        }
+    })
+    .catch(function(error) {
+        console.log(error.message);
+    });
+};
+
+function getWeather(weatherUrl, city) {
+    fetch(weatherUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        var date = moment().format("MMMM DD, YYYY");
+        var temperature = data.current.temp;
+        var humidity = data.current.humidity;
+        var wind = data.current.wind_speed;
+        var uvi = data.current.uvi;
+        var currentBox = document.getElementById('currentData');
+        document.getElementById('cityName').textContent = city + " (" + date + ")";
+        //append temp to current data list
+        var temp = document.createElement('li');
+        temp.textContent = "Temp: " + temperature + "\u00B0F"
+        currentBox.appendChild(temp);
+        //append wind to current data
+        var windSpeed = document.createElement('li');
+        windSpeed.textContent = "Wind: " + wind + " MPH";
+        currentBox.appendChild(windSpeed);
+        //append humidity to current data
+        var hum = document.createElement('li');
+        hum.textContent = "Humidity: " + humidity + "%";
+        currentBox.appendChild(hum);
+        //append UV to current data
+        var uvIndex = document.createElement('li');
+        uvIndex.textContent = "UV Index: " + uvi;
+        currentBox.appendChild(uvIndex);
+    })
+};
+
+//call standalone functions
+displaySearches();
